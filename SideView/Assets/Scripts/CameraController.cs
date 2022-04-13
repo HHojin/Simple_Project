8d154dbd697d175b91fbf773537be32f38a4d6cd
HHojin <hogin0616@gmail.com>
2022-04-14 04:43:12 +0900

    2020.1.3f1

---
+++ SideView/Assets/Scripts/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    [SerializeField]
+    private Vector3 mousePos;
+    public float moveSpeed = 20.0f;
+    public float zoomSpeed = 10.0f;
+
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    void Update()
+    {
+        Move();
+        Zoom();
+    }
+
+    private void Move()
+    {
+        mousePos = mainCamera.ScreenToViewportPoint(Input.mousePosition); // Viewport Point
+
+        if(Input.GetKey(KeyCode.W) || mousePos.y >= 0.99f) // 위로 카메라 이동
+        {
+            Debug.Log("Press W");
+            transform.Translate(moveSpeed * Time.deltaTime * Vector3.up, Space.World);
+        }
+        if (Input.GetKey(KeyCode.S) || mousePos.y <= 0.01f) // 아래로 카메라 이동
+        {
+            Debug.Log("Press S");
+            transform.Translate(moveSpeed * Time.deltaTime * Vector3.down, Space.World);
+        }
+        if (Input.GetKey(KeyCode.A) || mousePos.x <= 0.01f) // 왼쪽으로 카메라 이동
+        {
+            Debug.Log("Press A");
+            transform.Translate(moveSpeed * Time.deltaTime * Vector3.left, Space.World);
+        }
+        if (Input.GetKey(KeyCode.D) || mousePos.x >= 0.99f) // 오른쪽으로 카메라 이동
+        {
+            Debug.Log("Press D");
+            transform.Translate(moveSpeed * Time.deltaTime * Vector3.right, Space.World);
+        }
+    }
+
+    private void Zoom()
+    {
+        float distance = Input.GetAxis("Mouse ScrollWheel") * -1 * zoomSpeed;
+        if (distance != 0)
+        {
+            mainCamera.fieldOfView += distance;
+        }
+    }
+}
---
+++ SideView/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum State
+    {
+        Idle,
+        Walk,
+        Run,
+    };
+
+    private NavMeshAgent agent;
+    private Vector3 targetPos;
+
+    private bool isMove = false;
+    public float doubleClickSecond = 0.25f;
+    private bool isOneClick = false;
+    private double timer = 0;
+
+    private Animator anim;
+    //private Collider coll;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        //coll = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        FindDestination();
+
+        if (isOneClick && ((Time.time - timer) > doubleClickSecond))
+        {
+            isOneClick = false;
+        }
+
+        if (isMove)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (!isOneClick)
+                {
+                    //Debug.Log("One Click");
+                    timer = Time.time;
+                    isOneClick = true;
+
+                    // 한번 클릭(walk)
+                    anim.SetInteger("moveType", (int)State.Walk);
+                    agent.speed = 7;
+                }
+                else if (isOneClick && ((Time.time - timer) < doubleClickSecond))
+                {
+                    //Debug.Log("Double Click");
+                    isOneClick = false;
+
+                    // 두번 클릭(run)
+                    anim.SetInteger("moveType", (int)State.Run);
+                    agent.speed = 14;
+                }
+
+                agent.destination = targetPos;
+            }
+        }
+
+        if (DestinationArrived()) // 목적지에 도착했을 떄 Idle animation 실행
+        {
+            anim.SetInteger("moveType", (int)State.Idle);
+            isMove = false;
+        }
+    }
+
+    private void FindDestination()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.rigidbody != null)
+            {
+                //Debug.Log("Coll hit!");
+                targetPos = new Vector3(hit.point.x, hit.point.y, this.transform.position.z);
+
+                isMove = true;
+            }
+            else
+            {
+                isMove = false;
+            }
+        }
+    }
+
+    private bool DestinationArrived()
+    {
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
